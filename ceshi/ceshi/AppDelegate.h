5c029c3befaedd3c988a6349da467453d11ca91a
MuChen <752572065@qq.com>
2016-05-16 18:26:11 +0800

    ceshi

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by Student04 on 16/5/16.
+//  Copyright © 2016年 MuChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
